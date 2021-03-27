ca3692fbbdf3f79b3bf1b1bcd596d8d0b2946123
Shruti <shrutigupta5555@gmail.com>
2021-03-27 23:23:55 +0530

    added rendering of html entities

--- src/Components/PickColor.tsx
+++ src/Components/PickColor.tsx
@@ -1,22 +1,63 @@
 import React, { useState } from 'react';
 import { SketchPicker } from 'react-color';
 import './PickColor.css';
+import HtmlEntitySingle from './HtmlEntitySingle';
 
-function PickColor() {
+const htmlEntityList = [
+  {
+    element :'Heading',
+    color : '#000',
+  },
+  {
+    element :'Sub-Heading',
+    color : '#000',
+  },
+  {
+    element :'Paragraph',
+    color : '#000',
+  },
+  {
+    element :'Background color',
+    color : '#000',
+  },
+  
+];
+
+const HtmlEntity = () => {
+  
+  return <div className="html-elements">
+    {htmlEntityList.map((item) => {
+      // return  <h1>rendered in pc</h1>;
+       return (
+          <HtmlEntitySingle key={item.element} props={item} />
+       );
+    })};
+  </div>
+}
+  
+
+
+
+
+const  PickColor= () => {
   const [colorHexCode, setColorHexCode] = useState('#000000');
 
   return (
-    <div className="PickColor">
+    <div className="container">
+      <div className="pickColor">
       
       <SketchPicker
         color={colorHexCode}
         onChange={e => setColorHexCode(e.hex)} 
       />
 
-      
-     
+    </div>
+    <hr/>
+    
+    <HtmlEntity />  
 
     </div>
+    
   );
 }
 
